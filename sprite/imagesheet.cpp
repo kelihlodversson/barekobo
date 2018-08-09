41dd7d9662d40d328d0b367de1175edcd3daa413
Keli Hlodversson <keli@keli.dk>
2018-08-09 12:18:35 +0000

    Add a class for automatically chopping a sheet of images into an array of image arrays. The class takes information about the source data and the size of each subimage, and creates an array of Image instances from the parameters.
    Note that is assumes all subimages have the same size.

---
+++ sprite/imagesheet.cpp
@@ -0,0 +1,46 @@
+#include "sprite/imagesheet.h"
+#include "util/new.h" // for placement new
+
+using namespace hfh3;
+
+ImageSheet::ImageSheet(u8* inData, unsigned bufferWidth, unsigned bufferHeight, unsigned imageWidth, unsigned imageHeight, int transparent, unsigned inGroupSize) :
+    groupSize(inGroupSize)
+{
+    const unsigned columns = bufferWidth / imageWidth;
+    const unsigned rows = bufferHeight / imageHeight;
+    imageCount = rows*columns;
+
+    // Calculate the byte offset for a single row and column in the image data
+    const unsigned rowOffset = imageHeight * bufferWidth;
+    const unsigned colOffset = imageWidth;
+
+    // Since Image does not have a default constructor, we use placement new to initialize the elements in the array:
+    //      First allocate uninitialized memory big enough for the images
+    images = reinterpret_cast<Image*>(new u8[imageCount * sizeof(Image)]);
+    assert((reinterpret_cast<uintptr>(images) & 3) == 0); // assert that the allocated memory is aligned on 4 bytes
+
+    //      ... then loop through them and call the Image constructor in-place
+    for (unsigned row = 0, i=0; row < rows; row++)
+    {
+        for(unsigned col = 0; col < columns; col++, i++)
+        {
+            new(&images[i]) Image(&inData[rowOffset*row + colOffset*col], imageWidth, imageHeight, transparent, bufferWidth);
+        }
+    }
+}
+
+ImageSheet::~ImageSheet()
+{
+    if(images)
+    {
+        // Since we used placement new to allocate the array, we need to manually
+        // invoke the destructors for the images.
+        for(int i=0; i<imageCount; i++)
+        {
+            images[i].~Image();
+        }
+        // And then free the undelying array type
+        delete[] reinterpret_cast<u8*>(images);
+        images = nullptr;
+    }
+}
---
+++ sprite/imagesheet.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <circle/types.h>
+#include "sprite/image.h"
+
+namespace hfh3
+{
+
+    /** An image defines an area of pixels to be rendered to the screen.
+      * A single color can be defined as transparent, meaning that existing
+      * content in the framebuffer will be left untouched.
+      *
+      * Note that the Image class only keeps a reference to the underlying
+      * pixel buffer and that the actual rendering is handled by the ScreenManager.
+      */
+    class ImageSheet
+    {
+    public:
+        /** Initializes an image sheet from a rawy byte buffer. It assumes that
+          * all images in the sheet are of equal size and are tiled next to each other.
+          *
+          * @param inData The raw byte buffer containing a sheet of images.
+          * @param bufferWidth The number of bytes per line of the buffer image.
+          * @param bufferHeight The total number of scan lines in the buffer
+          * @param imageWidth The width of each image in the sheet
+          * @param imageHeight The height of each image in the sheet
+          * @param transparent The index of the transparent pixel. Pass -1 if all pixels should be rendered.
+          * @param groupSize The number of images in each group.
+          */
+        ImageSheet(u8* inData, unsigned bufferWidth, unsigned bufferHeight, unsigned imageWidth, unsigned imageHeight, int transparent=-1, unsigned groupSize=1);
+        ~ImageSheet();
+
+        /** Returns an array of images representing a group of images.
+          * Example: imageSheet[4][0] // Returns the first image in the 5th image group
+          */
+        class Image* operator[](int i)
+        {
+            return &images[groupSize*i];
+        }
+
+        int GetImageCount() const
+        {
+            return imageCount;
+        }
+
+        int GetGroupCount() const
+        {
+            return imageCount / groupSize;
+        }
+
+        int GetGroupSize() const
+        {
+            return groupSize;
+        }
+
+    private:
+        u8* GetImageStart(u8* data, int col, int row);
+        Image* images;
+        unsigned groupSize;
+        unsigned imageCount;
+    };
+}

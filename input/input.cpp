8931f91d8cadcac816c2ee665b3d4ae4e77322bf
Keli Hlodversson <keli@keli.dk>
2018-09-23 06:33:51 +0000

    intput/input.cpp got accidentally deleted in the previous change set

---
+++ input/input.cpp
@@ -0,0 +1,28 @@
+#include <circle/util.h>
+
+#include "input/input.h"
+using namespace hfh3;
+
+
+Input::Input()
+    : playerDirection(Direction::Stopped)
+{
+    memset(buttons, 0, sizeof(buttons));  
+}
+
+enum Input::ButtonState Input::GetButtonState(enum Input::Button button)
+{
+    enum ButtonState result = buttons[button];
+    buttons[button] = static_cast<ButtonState>(buttons[button] & ButtonDown); // Clear ButtonStateChanged
+    return result;
+}
+
+
+u8 Input::DumpInputState()
+{
+    return ( buttons[0]      & 1)    | 
+           ( buttons[1] << 1 & 2)    |
+           ( buttons[2] << 2 & 4)    |
+           ( buttons[3] << 3 & 8)    |
+           ((int)playerDirection)<<4 ; 
+}
